affabf5d8c40ab993ba9e8f6555d9713f2b7b8d8
Dmitry1517 <postbox404@gmail.com>
2025-12-19 02:44:51 +1000

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+export default function App() {
+  return (
+    <div className="app">
+      <div>Способ вывода</div>
+      <Button variant="contained" color="primary">Банки</Button>
+      <Button variant="contained" color="primary">Наличные</Button>
+      <Button variant="contained" color="primary">Курьер</Button>
+    </div>
+  )
+}
+
+
